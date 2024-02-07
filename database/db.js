0abae50ca1c8e4a0070c2010bba2f43968bfc513
Aneesh-Maurya <aneeshmaurya273@gmail.com>
2024-02-07 23:45:09 +0530

    initial

--- database/db.js
+++ database/db.js
@@ -1,22 +1,46 @@
-const pg=require('pg')
+const postgres = require('postgres');
  try{
-    var config = {
+    // var config = {
 
-        // for localsystem
+    //     // for localsystem
     
-        user: 'postgres', //env var: PGUSER 
-        database: 'employee', //env var: PGDATABASE 
-        password: 'Anee', //env var: PGPASSWORD 
-        host: 'localhost', // Server hosting the postgres database 
-        port: 5433, //env var: PGPORT 
-        max: 10000, // max number of clients in the pool 
-        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+    //    user: 'Aneesh-Maurya', //env var: PGUSER 
+    //     database: 'employee', //env var: PGDATABASE 
+    //     password: 't6ihJTbAG2LX', // t6ihJTbAG2LX  env var: PGPASSWORD 
+    //     host: 'ep-rough-scene-a5mcag2r-pooler.us-east-2.aws.neon.tech', // Server hosting the postgres database 
+    //     port: 5433, //env var: PGPORT 
+    //    max: 10000, // max number of clients in the pool 
+    //     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
           
-    }; 
-        const pool=new pg.Pool(config)
-        pool.connect().then(() => { console.log('Connected to PostgreSQL database!'); }) .catch((err) => { console.error('Not connecting to the database:'); });
-    
+    // }; 
+   
+    //     const pool=new pg.Pool(config)
+    //     pool.connect().then(() => { console.log('Connected to PostgreSQL database!'); }) .catch((err) => { console.error('Not connecting to the database:'); });
+    // app.js
+
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const config = {
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+};
+const sql=postgres(config)
+
+// async function getPgVersion() {
+//   const result = await sql`CREATE TABLE empData ( firstname varchar(80), lastname varchar(80), email varchar(80), mobile bigint, salary float )`;
+//   console.log(result);
+// }
+// getPgVersion()
+module.exports=sql
  }catch(err){
     console.log("db not connected",err)
  }
-module.exports=config
--- service.js
+++ service.js
@@ -3,7 +3,7 @@ const pg=require('pg')
 const app=express()
 const config=require("./database/db")
 
-const pool=new pg.Pool(config)
+
 
 module.exports={
 
@@ -12,37 +12,19 @@ module.exports={
         const {firstname,lastname,email,mobile,salary}=req.body
         // const client = await db.connect();
         // var query='CREATE TABLE empData ( firstname varchar(80), lastname varchar(80), email varchar(80), mobile bigint, salary float )' ID int NOT NULL,
-        var query="insert into empData(firstname,lastname,email,mobile,salary)values('" + firstname + "','" + lastname + "','" + email+ "','" + mobile+ "','" + salary + "')"
-        let connection = await pool.connect(); //gets connection from pool
-        if (connection) {
-          let result = await pool.query(query);//executes query and returns result
-          if (result) {
-            connection.release(); //releases the connection to the pool
-            res.send(result.rows)
-            console.log('sucess') //returns the result to the called function
-          } else {
-            return console.error('not releases the connection to the pool')
-          }
-        } else {
-          return console.error(' gets not connection from pool')
+        var query=await config`insert into empData(firstname,lastname,email,mobile,salary)values(${firstname},${lastname},${email},${mobile},${salary})`
+        if(query){
+          console.log(query)
+        }else{
+          console.log(err)
         }
     },
     GetData:async(req,res)=>{
-         var query='select *from empdata'
+         var query=await config`select *from empdata`
          // var query=  'CREATE TABLE empData ( ID SERIAL PRIMARY KEY ,firstname varchar(80), lastname varchar(80), email varchar(80), mobile bigint, salary float  )'
-        let connection = await pool.connect(); //gets connection from pool
-        if (connection) {
-          let result = await pool.query(query);//executes query and returns result
-          if (result) {
-            connection.release(); //releases the connection to the pool
-            res.send(result.rows)
-            console.log('sucess') //returns the result to the called function
-          } else {
-            return console.error('not releases the connection to the pool')
-          }
-        } else {
-          return console.error(' gets not connection from pool')
-        }
+       console.log(query)
+       res.send(query)
+      
     },
     update:async(req,res)=>{
       console.log(req.body)
