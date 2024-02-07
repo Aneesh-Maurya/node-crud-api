2a69e6a5a6558c3b5ef7dd5c121dba6e9c211cce
Aneesh-Maurya <aneeshmaurya273@gmail.com>
2024-02-07 22:23:14 +0530

    initial

--- database/db.js
+++ database/db.js
@@ -1,15 +1,22 @@
 const pg=require('pg')
-var config = {
+ try{
+    var config = {
 
-    // for localsystem
-
-    user: 'postgres', //env var: PGUSER 
-    database: 'employee', //env var: PGDATABASE 
-    password: 'Aneesh', //env var: PGPASSWORD 
-    host: 'localhost', // Server hosting the postgres database 
-    port: 5433, //env var: PGPORT 
-    max: 10000, // max number of clients in the pool 
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
-      
-};
+        // for localsystem
+    
+        user: 'postgres', //env var: PGUSER 
+        database: 'employee', //env var: PGDATABASE 
+        password: 'Anee', //env var: PGPASSWORD 
+        host: 'localhost', // Server hosting the postgres database 
+        port: 5433, //env var: PGPORT 
+        max: 10000, // max number of clients in the pool 
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+          
+    }; 
+        const pool=new pg.Pool(config)
+        pool.connect().then(() => { console.log('Connected to PostgreSQL database!'); }) .catch((err) => { console.error('Not connecting to the database:'); });
+    
+ }catch(err){
+    console.log("db not connected",err)
+ }
 module.exports=config
